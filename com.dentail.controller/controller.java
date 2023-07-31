3942c2991c79fca3b909058fc213cb5ee8bea486
naveenramesh <naveen.ramesh0@gmail.com>
2023-07-31 17:52:22 +0000

    added controller

---
+++ com.dentail.controller/controller.java
@@ -0,0 +1,12 @@
+/**
+ * controller
+ */
+public class controller {
+
+    public static void main(String[] args) {
+        int a,b;
+        a=Integer.parseInt(args[0]);
+        b=Integer.parseInt(args[1]);
+        System.out.println(a+b);
+    }
+}
