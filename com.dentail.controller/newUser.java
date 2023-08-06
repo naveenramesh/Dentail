0ef775aa1061efe52e481ddbb97ebb4682d4c9ff
riyosaki <naveen.ramesh0@gmail.com>
2023-08-06 18:09:15 +0530

    Removing unwanted classes

--- com.dentail.controller/newUser.java
+++
@@ -1,9 +0,0 @@
-public class newUser {
-    User user = new User();
-    public User setNewuser(String name, String address, String phone) {
-        user.setName(name);
-        user.setAddress(address);
-        user.setPhone(phone);
-        return user;
-    }
-}
